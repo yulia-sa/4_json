fc0cfe6bd224d90288d2629c6178f9cd483cdf94
Yulia Savanova <yulia@Yulias-iMac.local>
2018-11-12 00:17:00 +0300

    refactoring

--- pprint_json.py
+++ pprint_json.py
@@ -1,27 +1,42 @@
-import sys
 import json
-
-filepath = sys.argv[1]
 
 
 def load_data(filepath):
     try:
-        with open(filepath, 'r') as fp:
-            parsed_file = json.load(fp)
+        with open(filepath, 'r') as file_object:
+            parsed_file = json.load(file_object)
             return parsed_file
-
-    except (FileNotFoundError) as err:
-        print('Файл не найден!')
-    except (IOError, Exception) as err:
-        print(err)
+    
+    except (IOError, Exception):
+        return None
 
 
-def pretty_print_json(raw_json_data):
-    pretty_json = json.dumps(raw_json_data, indent=4,
-                             ensure_ascii=False, sort_keys=False)
+def print_pretty_json(raw_data):
+    pretty_json = json.dumps(
+        raw_data,
+        indent=4,
+        ensure_ascii=False,
+        sort_keys=False
+    )
     print(pretty_json)
 
 
 if __name__ == '__main__':
-    raw_json_data = load_data(filepath)
-    pretty_print_json(raw_json_data)
+    import os
+    import sys
+
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+
+        if not os.path.isfile(filepath):
+            print('Файл не найден!')
+        else:
+            raw_data = load_data(filepath)
+
+            if raw_data is None:
+                print('Произошла ошибка!')
+            else:
+                print_pretty_json(raw_data)
+
+    else:
+        print('Не задан путь к файлу!')
